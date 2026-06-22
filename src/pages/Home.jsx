eb2df988dc77c3454cb8bc02cac1938015b7db76
shella2796 <waramenashella@gmail.com>
2026-06-22 10:15:44 +0700

    Update Hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -46,16 +46,16 @@ export default function Home() {
               />
             </motion.h2>
             <motion.h2 variants={{ hidden: { opacity: 0, y: 10 }, visible: { opacity: 1, y: 0 } }}>
-              <span className="font-extrabold">Project</span>{" "}
+              <span className="font-extrabold">IT Project Manager,</span>{" "}
               <span
                 className="text-white font-extrabold"
                 style={{ WebkitTextStroke: "1px black" }}
               >
-                Manager
+                Driving Digital Products
               </span>
             </motion.h2>
             <motion.h2 variants={{ hidden: { opacity: 0, y: 10 }, visible: { opacity: 1, y: 0 } }}>
-              Based In <span className="font-extrabold">Indonesia.</span>
+              from <span className="font-extrabold">Idea to Launch.</span>
             </motion.h2>
           </motion.div>
 
